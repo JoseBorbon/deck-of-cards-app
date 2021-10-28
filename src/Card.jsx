f84911d813ebfe14d2a9c6a8084df8a8fce2876b
JoseBorbon <joserborbon@gmail.com>
2021-10-28 09:19:19 -0400

    add Card component

--- src/Card.jsx
+++ src/Card.jsx
@@ -3,7 +3,19 @@ import './Card.css';
 
 class Card extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <img
+          src={this.props.imageUrl}
+          alt={this.props.altText}
+          style={{
+            zIndex: this.props.zIdx,
+            transform:
+              this.props.zIdx % 2 === 0 ? 'rotate(20deg)' : 'rotate(50deg)',
+          }}
+        />
+      </div>
+    );
   }
 }
 
