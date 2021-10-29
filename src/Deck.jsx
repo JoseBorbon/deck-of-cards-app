d81abc6a0418c7353e1cc4c715a1a1988679b8f2
JoseBorbon <joserborbon@gmail.com>
2021-10-29 06:04:44 -0400

    refactor

--- src/Deck.jsx
+++ src/Deck.jsx
@@ -3,56 +3,58 @@ import axios from 'axios';
 import { v4 as uuid } from 'uuid';
 import Card from './Card';
 import './Deck.css';
+const API_BASE_URL = 'https://deckofcardsapi.com/api/deck/';
 
 class Deck extends Component {
   constructor(props) {
     super(props);
-    this.state = { url: '', remaining: null, drawnCards: [] };
+    this.state = { deck: null, drawnCards: [], isOut: false };
     this.drawCard = this.drawCard.bind(this);
   }
 
   async drawCard() {
-    if (this.state.remaining === 0) return;
-    let response = await axios.get(this.state.url);
-    //push object to drawnCards
+    if (this.state.drawnCards.length === 52) {
+      this.setState({ isOut: true });
+      return;
+    }
+    const { deck_id: id } = this.state.deck;
+    const card = await axios.get(`${API_BASE_URL + id}/draw/`);
     this.setState((st) => {
       return {
-        drawnCards: [...st.drawnCards, response.data],
-        remaining: response.data.remaining,
+        drawnCards: [...st.drawnCards, card.data],
       };
     });
   }
 
   async componentDidMount() {
-    //get the data based off url length in state
-    if (this.state.url.length === 0) {
-      //if empty use https://deckofcardsapi.com/api/deck/new/shuffle
-      let response = await axios.get(
-        'https://deckofcardsapi.com/api/deck/new/shuffle'
-      );
-      console.log(response.data);
-      //swap url in state to be https://deckofcardsapi.com/api/deck/${deck_id}/draw/
+    if (this.state.drawnCards.length === 0) {
+      let deck = await axios.get(`${API_BASE_URL}new/shuffle`);
       this.setState({
-        url: `https://deckofcardsapi.com/api/deck/${response.data.deck_id}/draw/`,
+        deck: deck.data,
       });
     }
   }
 
   render() {
     const cards = this.state.drawnCards.map(
-      ({ cards: [{ image, value, suit }] }) => (
+      ({ cards: [{ image, value, suit }], remaining }) => (
         <Card
           imageUrl={image}
           altText={`${value} of ${suit}`}
-          zIdx={-this.state.remaining}
+          zIdx={-remaining}
           key={uuid()}
         />
       )
     );
+    const button = this.state.isOut ? (
+      <button>GIMME MORE CARDS!</button>
+    ) : (
+      <button onClick={this.drawCard}>GIMME A CARD</button>
+    );
     return (
       <div>
         <div>
-          <button onClick={this.drawCard}>GIMME A CARD</button>
+          {button}
           <div className="Deck-cards">{cards}</div>
         </div>
       </div>
