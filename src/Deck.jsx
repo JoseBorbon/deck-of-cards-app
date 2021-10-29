9ad76958e8805d80f7393e106a30c78b85f90a70
JoseBorbon <joserborbon@gmail.com>
2021-10-29 08:29:50 -0400

    refactor drawCard with try catch instead of if

--- src/Deck.jsx
+++ src/Deck.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import { v4 as uuid } from 'uuid';
 import Card from './Card';
 import './Deck.css';
 const API_BASE_URL = 'https://deckofcardsapi.com/api/deck/';
@@ -12,20 +11,6 @@ class Deck extends Component {
     this.drawCard = this.drawCard.bind(this);
   }
 
-  async drawCard() {
-    if (this.state.drawnCards.length === 52) {
-      this.setState({ isOut: true });
-      return;
-    }
-    const { deck_id: id } = this.state.deck;
-    const card = await axios.get(`${API_BASE_URL + id}/draw/`);
-    this.setState((st) => {
-      return {
-        drawnCards: [...st.drawnCards, card.data],
-      };
-    });
-  }
-
   async componentDidMount() {
     if (this.state.drawnCards.length === 0) {
       let deck = await axios.get(`${API_BASE_URL}new/shuffle`);
@@ -35,26 +20,47 @@ class Deck extends Component {
     }
   }
 
+  async drawCard() {
+    try {
+      const { deck_id: id } = this.state.deck;
+      let card = await axios.get(`${API_BASE_URL + id}/draw/`);
+      if (!card.data.success) {
+        throw new Error('ALL OUT BUB!');
+      }
+      card = card.data.cards[0];
+      this.setState((st) => {
+        return {
+          drawnCards: [
+            ...st.drawnCards,
+            {
+              id: card.code,
+              image: card.image,
+              altText: `${card.value} of ${card.suit}`,
+            },
+          ],
+        };
+      });
+    } catch (err) {
+      this.setState({ isOut: true });
+    }
+  }
+
   render() {
-    const cards = this.state.drawnCards.map(
-      ({ cards: [{ image, value, suit }], remaining }) => (
-        <Card
-          imageUrl={image}
-          altText={`${value} of ${suit}`}
-          zIdx={-remaining}
-          key={uuid()}
-        />
-      )
-    );
-    const button = this.state.isOut ? (
-      <button>GIMME MORE CARDS!</button>
+    const cards = this.state.drawnCards.map(({ id, image, altText }) => (
+      <Card imageUrl={image} altText={altText} key={id} />
+    ));
+    const display = this.state.isOut ? (
+      <h1 className="Deck-all-out">All Out Bub!</h1>
     ) : (
-      <button onClick={this.drawCard}>GIMME A CARD</button>
+      <button className="Deck-button" onClick={this.drawCard}>
+        GIMME A CARD
+      </button>
     );
     return (
       <div>
         <div>
-          {button}
+          <h1 className="Deck-title">Card Dealer</h1>
+          {display}
           <div className="Deck-cards">{cards}</div>
         </div>
       </div>
